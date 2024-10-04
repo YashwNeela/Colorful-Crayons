c00e1171e766e20fa98a2f53a66d23fc6b70ee2a
YashwNeela <yashw@neelafilm.com>
2024-10-04 19:25:21 +0530

    Tip UI done

--- Assets/_SortingGame/_Scripts/UI/HUDUI.cs
+++ Assets/_SortingGame/_Scripts/UI/HUDUI.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 namespace TMKOC.Sorting
 {
     public class HUDUI : MonoBehaviour
     {
+        public Transform m_TipText;
+        private bool m_IsShowingTip;
         public void OnBackButtonClicked()
         {
             Gamemanager.Instance.GoBackToPlayschool();
@@ -14,6 +17,25 @@ namespace TMKOC.Sorting
         public void LevelCompleteCheck()
         {
             Gamemanager.Instance.LevelCompleteCheck();
+        }
+
+        public void ShowTip()
+        {
+            if(m_IsShowingTip)
+            {
+                CloseTip();
+                return;
+            }
+            m_TipText.DOLocalMoveX(0f, .25f);
+            m_IsShowingTip = true;
+            Invoke(nameof(CloseTip), 3f);
+        }
+
+        public void CloseTip()
+        {
+            m_TipText.DOLocalMoveX(445f, .25f);
+            m_IsShowingTip = false;
+
         }
     }
 }
