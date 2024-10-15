102ee759755dcd90c3b6ffb17611789f10bc1cc9
YashwNeela <yashw@neelafilm.com>
2024-10-15 15:24:54 +0530

    2 Levels done sucessfully

--- Assets/_SortingGame/_Scripts/CarSorting/CarTire.cs
+++ Assets/_SortingGame/_Scripts/CarSorting/CarTire.cs
@@ -10,21 +10,21 @@ namespace TMKOC.Sorting.CarSorting
 
         public CarType CarType => m_CarType;
 
-          protected virtual void OnTriggerEnter2D(Collider2D other)
-    {
-        HandleCollectorOnTriggerEnter(other);
-    }
+        protected virtual void OnTriggerEnter2D(Collider2D other)
+        {
+            HandleCollectorOnTriggerEnter(other);
+        }
 
-    /// <summary>
-    /// Sent each frame where another object is within a trigger collider
-    /// attached to this object (2D physics only).
-    /// </summary>
-    /// <param name="other">The other Collider2D involved in this collision.</param>
-    protected virtual void OnTriggerStay2D(Collider2D other)
-    {
-        HandleCollectorOnTriggerStay(other);
-    }
-    protected override void HandleCollectorOnTriggerEnter(Component collider)
+        /// <summary>
+        /// Sent each frame where another object is within a trigger collider
+        /// attached to this object (2D physics only).
+        /// </summary>
+        /// <param name="other">The other Collider2D involved in this collision.</param>
+        protected virtual void OnTriggerStay2D(Collider2D other)
+        {
+            HandleCollectorOnTriggerStay(other);
+        }
+        protected override void HandleCollectorOnTriggerEnter(Component collider)
         {
             base.HandleCollectorOnTriggerEnter(collider);
             Car collectorBox = null;
@@ -46,75 +46,76 @@ namespace TMKOC.Sorting.CarSorting
 
         }
 
-    protected virtual void OnTriggerExit2D(Collider2D other)
-    {
-        HandleCollectorOnTriggerExit(other);
-    }
-
-    protected override void HandleCollectorOnTriggerExit(Component collider)
-    {
-        base.HandleCollectorOnTriggerExit(collider);
-
-        
-        m_ValidCollector = null;
-        m_CurrentCollector = null;
-    }
-
-    protected override void OnPlacedCorrectly()
-    {
-        base.OnPlacedCorrectly();
-        
-    }
-
-    protected override void PlaceInCorrectly(Collector collector)
-    {
-        base.PlaceInCorrectly(collector);
-        
-    }
-
-    protected override void HandleDragEnd()
-    {
-        if(m_IsPlacedInsideCollector)
+        protected virtual void OnTriggerExit2D(Collider2D other)
         {
-            draggable.ResetToStartDraggingValues();
-            return;
+            HandleCollectorOnTriggerExit(other);
         }
-        if (m_CurrentCollector != null)
+
+        protected override void HandleCollectorOnTriggerExit(Component collider)
         {
-            if (m_ValidCollector != null){
-                
-                if((m_CurrentCollector as Car).CarType.HasFlag(m_CarType))
-                    m_CurrentCollector.SnapCollectibleToCollector(this, () => OnPlacedCorrectly());
-                else
-                    m_CurrentCollector.SnapCollectibleToCollector(this, () => {});
+            base.HandleCollectorOnTriggerExit(collider);
+
+
+            m_ValidCollector = null;
+            m_CurrentCollector = null;
+        }
+
+        protected override void OnPlacedCorrectly()
+        {
+            base.OnPlacedCorrectly();
+
+        }
+
+        protected override void PlaceInCorrectly(Collector collector)
+        {
+            base.PlaceInCorrectly(collector);
+
+        }
+
+        protected override void HandleDragEnd()
+        {
+            if (m_IsPlacedInsideCollector)
+            {
+                draggable.ResetToStartDraggingValues();
+                return;
+            }
+            if (m_CurrentCollector != null && m_CurrentCollector.IsSlotAvailable())
+            {
+                if (m_ValidCollector != null)
+                {
+
+                    if ((m_CurrentCollector as Car).CarType.HasFlag(m_CarType))
+                        m_CurrentCollector.SnapCollectibleToCollector(this, () => OnPlacedCorrectly());
+                    else
+                        m_CurrentCollector.SnapCollectibleToCollector(this, () => { });
                     PlaceInCorrectly(m_CurrentCollector);
 
+                }
+
+
             }
-        
-
         }
-    }
 
-    protected override void OnDragStartedStaticAction(Transform transform)
-    {
-        if(transform == this.transform)
-            return;
+        protected override void OnDragStartedStaticAction(Transform transform)
+        {
+            if (transform == this.transform)
+                return;
 
-       if(m_Collider is Collider)
-            ((Collider)m_Collider).enabled = false;
-        if(m_Collider is Collider2D)
-            ((Collider2D)m_Collider).enabled = false;
-    }
+            if (m_Collider is Collider)
+                ((Collider)m_Collider).enabled = false;
+            if (m_Collider is Collider2D)
+                ((Collider2D)m_Collider).enabled = false;
+        }
 
-    protected override void OnDragEndStaticAction(Transform transform)
-    {
-      if(transform == this.transform)
-            return;
+        protected override void OnDragEndStaticAction(Transform transform)
+        {
+            if (transform == this.transform)
+                return;
 
-       if(m_Collider is Collider)
-            ((Collider)m_Collider).enabled = true;
-        if(m_Collider is Collider2D)
-            ((Collider2D)m_Collider).enabled = true;
-    }
+            if (m_Collider is Collider)
+                ((Collider)m_Collider).enabled = true;
+            if (m_Collider is Collider2D)
+                ((Collider2D)m_Collider).enabled = true;
+        }
     }
 }
