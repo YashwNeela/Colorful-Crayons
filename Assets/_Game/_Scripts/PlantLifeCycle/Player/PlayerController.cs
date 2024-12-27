01e61c9c04c59edc6c7c03664739eda4d2b475da
YashwNeela <yashw@neelafilm.com>
2024-12-27 12:58:33 +0530

    Animations imported

--- Assets/_Game/_Scripts/PlantLifeCycle/Player/PlayerController.cs
+++ Assets/_Game/_Scripts/PlantLifeCycle/Player/PlayerController.cs
@@ -4,27 +4,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+
 namespace TMKOC.PlantLifeCycle
 {
+    [System.Serializable]
+    public enum PlayerState
+    {
+        Idle,
+        Walking,
+        Dancing,
+        WaitingToDig,
+        Digging
+    }
     public class PlayerController : MonoBehaviour
     {
+        public PlayerState m_CurrentPlayerState;
         [SerializeField]
         public UnityEvent OnPlayerReadyToDig;
 
         Animator m_Animator;
 
+        public Transform m_DigSoilPSParent;
+
         void Awake()
         {
             m_Animator = GetComponent<Animator>();
         }
+
+        public void SetPlayerState(PlayerState state)
+        {
+            m_CurrentPlayerState = state;
+
+        }
         public void PlayDigSoilAnimation()
         {
+
             m_Animator.SetTrigger("dig");
-        }
-
-        public void OnDigSoilAnimationFinished()
-        {
-
         }
 
         public void PlayerReadyToDig()
@@ -32,14 +47,9 @@ namespace TMKOC.PlantLifeCycle
             OnPlayerReadyToDig?.Invoke();
         }
 
-        public void PlayDigAnimation()
-        {
-
-        }
-
         public void PlayGroundDiggingParicleEffect()
         {
-            ParticleEffectManager.Instance.PlayParticleEffect(0,transform.position, Vector3.one,null);
+            ParticleEffectManager.Instance.PlayParticleEffect(0,m_DigSoilPSParent.position, Vector3.one,null);
         }
     }
 }
