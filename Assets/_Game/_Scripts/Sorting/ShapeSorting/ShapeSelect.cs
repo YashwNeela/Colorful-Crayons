18a31ad9f5db36595f136def2be420d55c6a9d4a
YashwNeela <yashw@neelafilm.com>
2026-06-26 14:19:53 +0530

    shape sorting level one done

--- Assets/_Game/_Scripts/Sorting/ShapeSorting/ShapeSelect.cs
+++ Assets/_Game/_Scripts/Sorting/ShapeSorting/ShapeSelect.cs
@@ -64,7 +64,7 @@ public class ShapeSelect : Collectible
         {
           //  m_SelectedSequence.Play();
           m_HighLightGameobject.SetActive(true);
-            transform.DOScale(110f, 0.25f);
+            transform.DOScale(80f, 0.25f);
             OnShapeSelected?.Invoke(m_ShapeType);
         }
 
@@ -73,7 +73,7 @@ public class ShapeSelect : Collectible
           m_HighLightGameobject.SetActive(false);
 
          //   m_SelectedSequence.PlayBackwards();
-            transform.DOScale(100f, 0.25f);
+            transform.DOScale(68f, 0.25f);
             OnShapeDeselected?.Invoke(m_ShapeType);
         }
 
